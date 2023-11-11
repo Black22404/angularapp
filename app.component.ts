517f7638b87cc7d17f69d63b904d6f5d9504a71a
Donya Hassan <139344624+Black22404@users.noreply.github.com>
2023-11-11 22:36:13 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SlidebarComponent } from "./slidebar/slidebar.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, NavbarComponent, SlidebarComponent]
+})
+export class AppComponent {
+  title = 'Vnew';
+}
